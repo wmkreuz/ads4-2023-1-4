a46b6d0315163f914e6c3ad1a3f10d2a3318c3fb
Willhan Kreuz <willhan.kreuz@santerra.local>
2023-04-06 13:25:36 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'venda-recibo';
+  
+  public cliente:string = '';
+
+  public produto:string = '';
+
+  public qtde:number = 0;
+
+  public valor:number = 0;
+
+  public show: string = '';
+
+  public recibo: string = '';
+
+  incrementar():number{
+    return this.qtde += 1;
+  }
+
+  decrementar():number{
+    if( !((this.qtde -= 1) <= 0) ){
+      return this.qtde -= 1;
+    } else {
+      return this.qtde = 0;
+    }
+    
+  }
+
+  visualizarRecibo() {
+    this.show = 'Sim'
+    this.recibo = '******************************************\n' +
+                  'RECIBO DE VENDA\n' +
+                  '******************************************\n' +
+                  'Cliente...........: ' + this.cliente +
+                  '\nProduto...........: ' + this.produto +
+                  '\nQuantidade........: ' + this.qtde + 'x - R$' + this.valor + '.00' +
+                  '\n__________________________________________\n' +
+                  '\nValor Total (R$)..: R$' + this.valor * this.qtde + '.00';
+  }
+
 }
